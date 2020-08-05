f2320150a2926b2dae48ed22d9fe3fd21e3c1692
Hush <rajputsiddharth18@gmail.com>
2020-08-05 20:56:27 +0530

    added async prg

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: LoadingScreen(),
     );
---
+++ lib/services/location.dart
@@ -0,0 +1,21 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location
+{
+  double latitude;
+  double longititude;
+
+  Future<void> getCurrentLocation()
+  async {
+    try{
+      Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      print(position);
+      latitude = position.latitude;
+      longititude = position.longitude;
+    }
+    catch(e)
+    {
+      print(e);
+    }
+  }
+}
