9014058ef3ca005a730ee354d22d8dbba21f49dd
Hush <rajputsiddharth18@gmail.com>
2020-08-07 01:12:44 +0530

    added any location weather api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:weather/services/location.dart';
 import 'package:http/http.dart';
+
+const apikey = 'cc07b30bfc796f97f8c6ceb318d38cf8';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,21 +12,38 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  double latitude ;
+  double longitude ;
+
+
   void getLocation()
   async{
     Location location = Location();
     await location.getCurrentLocation();
     print(location.latitude);
     print(location.longititude);
+    latitude = location.latitude;
+    longitude = location.longititude;
+    getData();
   }
 
   void getData()
   async{
-    Response response = await get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cc07b30bfc796f97f8c6ceb318d38cf8");
+    Response response = await get("https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apikey");
     if(response.statusCode == 200)
       {
         String data = response.body;
         print(data);
+        var coordinatelong = jsonDecode(data)['coord']['lon'];
+        var coordinatelat = jsonDecode(data)['coord']['lat'];
+        var weatherdata = jsonDecode(data)['weather'][0]['description'];
+        var temprature = jsonDecode(data)['main']['temp'];
+        var cityname = jsonDecode(data)['name'];
+        print(coordinatelong);
+        print(coordinatelat);
+        print(weatherdata);
+        print(temprature);
+        print(cityname);
       }
     else{
       print(response.statusCode);
@@ -34,7 +54,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
-    getData();
   }
 
   @override
